49648cce4d913c79fd8d4330e94810d54e3badac
Carla Domingo <carladomingomiquel@gmail.com>
2026-04-13 10:05:00 +0200

    feat: add design at login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,20 +4,172 @@ import Link from "next/link";
 //className={css({})}
 export default function Login() {
 	return (
-		<div>
-			<h2>Iniciar Sesión</h2>
-			<input type="email" placeholder="Correo electrónico"></input>
-			<input type="password" placeholder="Contraseña"></input>
-			<p>
-				¿Has olvidado la contraseña?
-				<Link href="/recuperar-contraseña">Recuperar contraseña</Link>
-			</p>
-			<button>Acceder</button>
-			<p>
+		<div
+			className={css({
+				display: "flex",
+				flexDirection: "column",
+				gap: "4",
+				alignItems: "center",
+				justifyContent: "center",
+				margin: "100px",
+			})}
+		>
+			<div
+				className={css({
+					border: "1px solid #F8BDBE",
+					borderRadius: "8px",
+					padding: {
+						base: "40px",
+						md: "50px",
+					},
+
+					display: "flex",
+					flexDirection: "column",
+					gap: "4",
+					alignItems: "center",
+					justifyContent: "center",
+				})}
+			>
+				<h2
+					className={css({
+						fontFamily: "fraunces",
+						fontWeight: "600",
+						fontSize: "20",
+					})}
+				>
+					Iniciar Sesión
+				</h2>
+				<input
+					type="email"
+					placeholder="Correo electrónico"
+					className={css({
+						backgroundColor: "#FAF8F8",
+						border: "1px solid #EDE5E3",
+						borderRadius: "8px",
+						padding: "12px 16px",
+						fontSize: "14px",
+						fontFamily: "dmSans",
+						color: "#1A0A08",
+						outline: "none",
+						width: {
+							base: "300px",
+							md: "500px",
+						},
+						_placeholder: {
+							color: "#AA8880",
+						},
+						_focus: {
+							borderColor: "#E03020",
+						},
+					})}
+				></input>
+				<input
+					type="password"
+					placeholder="Contraseña"
+					className={css({
+						backgroundColor: "#FAF8F8",
+						border: "1px solid #EDE5E3",
+						borderRadius: "8px",
+						padding: "12px 16px",
+						fontSize: "14px",
+						fontFamily: "dmSans",
+						color: "#1A0A08",
+						outline: "none",
+						width: {
+							base: "300px",
+							md: "500px",
+						},
+						_placeholder: {
+							color: "#AA8880",
+						},
+						_focus: {
+							borderColor: "#E03020",
+						},
+					})}
+				></input>
+				<p
+					className={css({
+						fontSize: "8px",
+						color: "#1A0A08",
+						textAlign: "center",
+						width: "100%",
+						fontFamily: "dmSans",
+					})}
+				>
+					¿Has olvidado la contraseña?
+					<Link
+						href="/recuperar-contraseña"
+						className={css({
+							fontSize: "8px",
+							color: "#1A0A08",
+							fontWeight: "700",
+							textAlign: "center",
+							width: "100%",
+							fontFamily: "dmSans",
+						})}
+					>
+						{" "}
+						Recuperar contraseña
+					</Link>
+				</p>
+				<button
+					className={css({
+						backgroundColor: "#E6322B",
+						padding: "10px 20px",
+						borderRadius: "5px",
+						color: "#FFFFFF",
+						fontFamily: "dmSans",
+						fontSize: "12",
+						border: "none",
+						marginTop: "20px",
+					})}
+				>
+					Acceder
+				</button>
+			</div>
+			<p
+				className={css({
+					fontSize: "10px",
+					color: "#1A0A08",
+					textAlign: "center",
+					width: "100%",
+					fontFamily: "dmSans",
+					margin: "20px 0",
+				})}
+			>
 				¿Aún no te has registrado?{" "}
-				<Link href="/registro">Regístrate aqui</Link>
+				<Link
+					href="/registro"
+					className={css({
+						fontSize: "10px",
+						color: "#1A0A08",
+						fontWeight: "700",
+						textAlign: "center",
+						width: "100%",
+						fontFamily: "dmSans",
+						display: {
+							base: "block",
+							md: "inline",
+						},
+					})}
+				>
+					Regístrate aqui
+				</Link>
 			</p>
-			<div>
+			<div
+				className={css({
+					backgroundColor: "#F8BDBE",
+
+					borderRadius: "8px",
+					padding: "12px 16px",
+					fontSize: "10px",
+					textAlign: "center",
+					fontFamily: "dmSans",
+					color: "#1A0A08",
+					outline: "none",
+					width: "300px",
+				})}
+			>
 				<p>Correo electrónico o contraseña incorrectos</p>
 			</div>
 		</div>
