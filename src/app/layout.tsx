15fb94cfa99eda781aead9bf335de8907eb6544f
Carla Domingo <carladomingomiquel@gmail.com>
2026-05-14 23:55:41 +0200

    feat(layout): add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans, Fraunces } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
+const dmSans = DM_Sans({
 	subsets: ["latin"],
+	variable: "--font-dm-sans",
 });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+const fraunces = Fraunces({
 	subsets: ["latin"],
+	variable: "--font-fraunces",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +26,7 @@ export default function RootLayout({
 	return (
 		<html
 			lang="en"
-			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+			className={`${dmSans.variable} ${fraunces.variable} h-full antialiased`}
 		>
 			<body className="min-h-full flex flex-col">{children}</body>
 		</html>
