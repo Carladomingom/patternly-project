96de02595fa54fafb3615e70d5556bfc86701846
Carla Domingo <carladomingomiquel@gmail.com>
2026-05-24 15:12:52 +0200

    feat: add metadata

---
+++ src/app/components/nav/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { css } from "../../../../styled-system/css";
+import { useState } from "react";
+
+type NavbarProps = {
+	isLogged: boolean;
+};
+
+export default function Nav({ isLogged }: NavbarProps) {
+	const [open, setOpen] = useState(false);
+
+	return (
+		<header
+			className={css({
+				width: "100%",
+				borderBottom: "1px solid",
+				borderColor: "gray.200",
+				px: "6",
+				py: "4",
+			})}
+		>
+			<nav>
+				{/* Desktop version */}
+				<div
+					className={css({
+						display: {
+							base: "none",
+							md: "flex",
+						},
+
+						alignItems: "center",
+						justifyContent: "space-between",
+					})}
+				>
+					<Link href="/" className="flex-1 w-14">
+						<Image
+							src="/logo.svg"
+							alt="Logo"
+							width={140}
+							height={40}
+						/>
+					</Link>
+					<div className="gap-8 flex">
+						<Link href="/comunidad">Comunidad</Link>
+
+						<Link href="/crear-patron">Crear patrón</Link>
+						{isLogged ? (
+							<Link href="/mi-cuenta">
+								<Image
+									src="/login-icon.svg"
+									alt="Login Icon"
+									width={20}
+									height={20}
+								/>
+							</Link>
+						) : (
+							<Link href="/login"> Iniciar Sesión </Link>
+						)}
+					</div>
+				</div>
+				{/* Mobile version */}
+
+				<div
+					className={css({
+						display: {
+							base: "grid",
+							md: "none",
+						},
+
+						gridTemplateColumns: "1fr auto 1fr",
+						alignItems: "center",
+					})}
+				>
+					<button
+						onClick={() => setOpen(!open)}
+						className={css({
+							justifySelf: "start",
+							border: "none",
+							background: "transparent",
+						})}
+					>
+						<Image
+							src="/menu-icon.svg"
+							alt="Menu"
+							width={30}
+							height={30}
+						/>
+					</button>
+
+					<Link
+						href="/"
+						className={css({
+							justifySelf: "center",
+						})}
+					>
+						<Image
+							src="/logo-mobile.svg"
+							alt="logo-mobile"
+							width={40}
+							height={40}
+						/>
+					</Link>
+
+					<div
+						className={css({
+							justifySelf: "end",
+						})}
+					>
+						login
+					</div>
+				</div>
+
+				{open && (
+					<div
+						className={css({
+							display: {
+								base: "flex",
+								md: "none",
+							},
+
+							flexDirection: "column",
+							gap: "4",
+							mt: "4",
+						})}
+					>
+						<Link href="/comunidad">Comunidad</Link>
+
+						<Link href="/crear-patron">Crear patrón</Link>
+					</div>
+				)}
+			</nav>
+		</header>
+	);
+}
