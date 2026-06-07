8c4c65cb6d2c5aa865291ddbb20e838a1bbf4bab
Carla Domingo <carladomingomiquel@gmail.com>
2026-06-07 23:44:55 +0200

    fix(patron): fix styled-system import path

--- src/app/patron/[id]/page.tsx
+++ src/app/patron/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
-import { css } from "../../../styled-system/css";
+import { css } from "../../../../styled-system/css";
 import { PatronClient } from "./PatronClient";
 import type { Pattern } from "@/types";
 
